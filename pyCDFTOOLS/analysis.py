d5a566c3568b70275355c43fa45a0b6a1c614cdd
Julian Mak <julian.c.l.mak@googlemail.com>
2023-11-25 17:09:00 +0800

    80 chars

--- pyCDFTOOLS/analysis.py
+++ pyCDFTOOLS/analysis.py
@@ -5,7 +5,8 @@ Module to house the subroutines that focuses on analysing the data. Naming to
 mirror CDFTOOLS (e.g. cdfmoc computes the moc at fixed height etc.)
 """
 
-# TODO: only going to write a few subroutines, in order to see how imbuement and coding structure is going to work
+# TODO: only going to write a few subroutines, in order to see how imbuement and 
+#       coding structure is going to work
 
 #-------------------------------------------------------------------------------
     
