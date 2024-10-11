c7f0efc99fb4573963cd735615d47af082369c31
Julian Mak <julian.c.l.mak@googlemail.com>
2024-10-11 16:42:47 +0800

    cdfmoc

--- pyCDFTOOLS/analysis.py
+++ pyCDFTOOLS/analysis.py
@@ -23,12 +23,14 @@ def cdfcurl(grid, ds, un, vn, jk=0, **bd):
     a relative vorticity. Modify this as appropriate if need be.
     """
 
-    # this is put onto the F-points
+    # 1) calculation, puts the output onto the F-points
     socurl  = (  grid.diff(vn * ds.e2v, "X", **bd) 
                - grid.diff(un * ds.e1u, "Y", **bd) 
               ) / (ds.e1f * ds.e2f) * ds.fmask[jk, :, :]
                
-    # imbue some useful attributes
+    # imbue some useful variables/attributes
+    socurl["glamf"] = ds.glamf
+    socurl["gphif"] = ds.gphif
     socurl["gdept_1d"] = ds.gdept_1d[jk]
     socurl.attrs["standard_name"] = "socurl"
     socurl.attrs["long_name"]     = "Relative_vorticity (curl)"
@@ -37,3 +39,35 @@ def cdfcurl(grid, ds, un, vn, jk=0, **bd):
     return socurl
     
 #-------------------------------------------------------------------------------
+
+def cdfmoc(grid, ds, vn, **bd):
+    """Computes the meridional overturning circulation in depth co-ordinates,
+       in a way consistent with CDFTOOLS/cdfmoc
+    
+    Expects a 3d field input. Computes this along ALL time by default; this is 
+    not a necessarily a problem until evaluation.
+    
+    *** NOTE ***
+    By default attributes assume the inputs are *velocities*, and the output is
+    a transport. Modify this as appropriate if need be.
+    """
+    
+    # input is (z, y, x), grid_V, units of m s-1
+
+    # 1) zonal integral and multiply by e3v; (z, y), grid_V and units of m3 s-1
+    moc = (vn * ds.e1v * ds.e3v_0).sum(dim="x_c")
+
+    # 2) cumulative sum in k from TOP; sum in Z puts it onto z_f, units of Sv
+    #    then removes the total integral (the last entry)
+    moc = grid.cumsum(moc, axis="Z", **bd) / 1e6
+    
+    moc -= moc.isel({"z_f" : -1})
+    
+    # imbue some useful variables/attributes
+    moc["gphiv"] = ds.gphiv[:, 1]  # placeholder only
+    moc["gdepw_1d"] = ds.gdepw_1d
+    moc.attrs["standard_name"] = "moc"
+    moc.attrs["long_name"]     = "Meridional Overturning Circulation (avg at fixed depth)"
+    moc.attrs["units"]         = "Sv"
+
+    return moc
